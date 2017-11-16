5d93183fed4d51ffc20e7423520441e44669fe19
Capelliexp <the.1man@hotmail.com>
2017-11-16 19:44:42 +0100

    Nsight setup

--- CUDA_Sorting/Source.cu
+++ CUDA_Sorting/Source.cu
@@ -14,12 +14,12 @@ int* SortCUDA(int* data);
 int* SortCPU(int* data);
 void SortElements(int* data, int pos, int mod);
 void CheckDataOrder(int dataSorted[]);
-void PrintError(int zone, cudaError_t* blob);
+void PrintError(std::string zone, cudaError_t* blob);
 
 #define DATA_AMOUNT 200
-#define BLOCK_AMOUNT 8
-#define THREADS_PER_BLOCK 16
-#define THREADS_PER_GRID (BLOCK_AMOUNT*THREADS_PER_BLOCK)	//max 131072 ???
+#define BLOCK_AMOUNT 10
+#define THREADS_PER_BLOCK 10
+#define THREADS_IN_GRID (BLOCK_AMOUNT*THREADS_PER_BLOCK)	//max 131072 ???
 
 const dim3 blockSize = dim3(BLOCK_AMOUNT, 1, 1);
 const dim3 threadsPerBlock = dim3(THREADS_PER_BLOCK, 1, 1);
@@ -35,6 +35,7 @@ void OddEvenSort(int* data_d, int n) {
 
 		for (int i = 0; i < n; ++i) {
 			pos = id + mod;
+			__syncthreads;
 			if (data_d[pos] > data_d[pos + 1]) {
 				int temp = data_d[pos];
 				data_d[pos] = data_d[pos + 1];
@@ -42,7 +43,6 @@ void OddEvenSort(int* data_d, int n) {
 			}
 			if (mod == 0) mod = 1;
 			else mod = 0;
-			__syncthreads;
 		}
 	}
 }
@@ -60,10 +60,9 @@ int main(int argc, char* argv[]) {
 	CheckDataOrder(dataSortedByCUDA);
 
 	/*std::cout << std::endl;
-	
 	dataSortedByCPU = SortCPU(data);	
-	CheckDataOrder(dataSortedByCPU);
-	*/
+	CheckDataOrder(dataSortedByCPU);*/
+
 	std::cout << std::endl << "fin" << std::endl;
 	getchar();
 	return 0;
@@ -73,10 +72,19 @@ int main(int argc, char* argv[]) {
 void PrintGenInfo() {
 	SYSTEM_INFO siSysInfo;
 	GetSystemInfo(&siSysInfo);
+	cudaError_t blob;
 
-	std::cout << "DATA_AMOUNT: " << DATA_AMOUNT << std::endl <<
+	int deviceCount = 0, setDevice = 0;
+	if (cudaGetDeviceCount(&deviceCount) != cudaSuccess) PrintError("Get1", &blob);
+	if (cudaSetDevice(setDevice) != cudaSuccess) PrintError("Set1", &blob);
+
+	std::cout <<
+		"DATA_AMOUNT: " << DATA_AMOUNT << std::endl <<
 		"BLOCK_AMOUNT: " << BLOCK_AMOUNT << std::endl <<
-		"THREADS_PER_BLOCK: " << THREADS_PER_BLOCK << std::endl;
+		"THREADS_PER_BLOCK: " << THREADS_PER_BLOCK << std::endl <<
+		std::endl << "Available devices: " << deviceCount << std::endl <<
+		"Device in use: " << setDevice << std::endl;
+
 
 	if ((BLOCK_AMOUNT*THREADS_PER_BLOCK * 2) < DATA_AMOUNT)
 		std::cout << std::endl << "!!! WARNING: not enough threads to cover data length !!!" << std::endl;
@@ -104,20 +112,19 @@ void FillArray(int data[]) {
 //SORT DATA WITH CUDA USING ODD-EVEN SORTING
 int* SortCUDA(int* data) {
 	std::clock_t start;
-	double duration;
 	int* dataSorted = new int[DATA_AMOUNT];
 	int* data_d = 0;
 	cudaError_t blob;
 
-	if ((blob = cudaMalloc((void**)&data_d, (DATA_AMOUNT+1) * sizeof(int))) != cudaSuccess) PrintError(0, &blob);
-	if ((blob = cudaMemcpy(data_d, data, (DATA_AMOUNT+1) * sizeof(int), cudaMemcpyHostToDevice)) != cudaSuccess) PrintError(1, &blob);
+	if ((blob = cudaMalloc((void**)&data_d, (DATA_AMOUNT+1) * sizeof(int))) != cudaSuccess) PrintError("Mal1", &blob);
+	if ((blob = cudaMemcpy(data_d, data, (DATA_AMOUNT+1) * sizeof(int), cudaMemcpyHostToDevice)) != cudaSuccess) PrintError("Cpy1", &blob);
 
 	start = std::clock();	//timer start
-	OddEvenSort<<<blockSize, threadsPerBlock>>> (data_d, (int)DATA_AMOUNT);
-	duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;	//timer stop
+	OddEvenSort<<<blockSize, threadsPerBlock>>> (data_d, DATA_AMOUNT);
+	double duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;	//timer stop
 
-	if ((blob = cudaMemcpy(dataSorted, data_d, (DATA_AMOUNT) * sizeof(int), cudaMemcpyDeviceToHost)) != cudaSuccess) PrintError(2, &blob);
-	if ((blob = cudaFree(data_d)) != cudaSuccess) PrintError(3, &blob);
+	if ((blob = cudaMemcpy(dataSorted, data_d, (DATA_AMOUNT) * sizeof(int), cudaMemcpyDeviceToHost)) != cudaSuccess) PrintError("Cpy2", &blob);
+	if ((blob = cudaFree(data_d)) != cudaSuccess) PrintError("Free1", &blob);
 
 	std::cout << "CUDA sorting time: " << duration << " sec" << std::endl;
 	return dataSorted;
@@ -126,19 +133,16 @@ int* SortCUDA(int* data) {
 //SORT DATA WITH CPU USING ODD-EVEN SORTING
 int* SortCPU(int* data) {
 	std::clock_t start;
-	double duration;
 	int* dataSorted = data;
 	int mod;
 
 	start = std::clock();	//timer start
-
 	for (int i = 0; i < DATA_AMOUNT; ++i) {	//will run DATA_AMOUNT times
 		mod = i%2;
 		for (int j = 0; j < DATA_AMOUNT; j +=2)	//will run DATA_AMOUNT/2 times
 			SortElements(dataSorted, j, mod);
 	}
-
-	duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;	//timer stop
+	double duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;	//timer stop
 
 	std::cout << "CPU sorting time: " << duration << " sec" << std::endl;
 	return dataSorted;
@@ -184,25 +188,7 @@ void CheckDataOrder(int dataSorted[]) {
 }
 
 //CUDA ERROR TYPE PRINT 
-void PrintError(int zone, cudaError_t* blob) {
-	std::cout << "ERROR: ";
-	switch (zone) {
-	case 0:
-		std::cout << "MALLOC 1 - ";
-		break;
-	case 1:
-		std::cout << "MEMCPY 1 - ";
-		break;
-	case 2:
-		std::cout << "MEMCPY 2 - ";
-		break;
-	case 3:
-		std::cout << "FREE 1 - ";
-		break;
-	default:
-		std::cout << "<unhandled zone> - ";
-		break;
-	}
-	std::cout << cudaGetErrorString(*blob) << std::endl;
+void PrintError(std::string zone, cudaError_t* blob) {
+	std::cout << "ERROR: " << zone << cudaGetErrorString(*blob) << std::endl;
 	*blob = cudaDeviceReset();
 }
